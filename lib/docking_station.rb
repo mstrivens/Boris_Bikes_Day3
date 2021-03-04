eef72471d964091893fe49a3f40fb83dbc4ff82f
Natalia_Sherchenkova <tataterer@gmail.com>
2021-03-04 16:59:45 +0200

    Add docking method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,9 @@ class DockingStation
   def release_bike
     @bike = Bike.new
   end
+  def dock_bike(bike)
+    @bike = bike
+  end
 end
 
 class Bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,20 @@ describe DockingStation do
       bike = subject.release_bike
       expect(bike).to be_working
     end
+
+    it 'docks bike' do
+    expect(subject).to respond_to(:dock_bike)
+  end
+
+   it 'shows bike' do
+     expect(subject.dock_bike(:bike)).to eq(:bike)
+   end
 end
+
+# As a member of the public
+# So I can return bikes I've hired
+# I want to dock my bike at the docking station
+
+# As a member of the public
+# So I can decide whether to use the docking station
+# I want to see a bike that has been docked
